248013be4d58b00e1bf43887c359ee89f1d88f88
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2021-01-02 21:06:17 +0100

    :lipstick: Update ui transition theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import styles from './App.module.css';
 import Header from './components/Header';
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
   function changeTheme() {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
   }
 
   return (
-    <div className={`${styles.app} ${styles[theme]}`}>
-      <Header {... { theme, changeTheme }} />
+    <div className={`app ${theme}`}>
+      <div className="banner" />
+      <div className="container">
+        <Header {...{ theme, changeTheme }} />
+      </div>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import moonIcon from '../../images/icon-moon.svg';
+import sunIcon from '../../images/icon-sun.svg';
+
 import styles from './Header.module.css';
 
 const Header = ({ theme, changeTheme }) => (
-  <div className={`${styles.background} ${styles[theme]}`}>
-    <div className="container">
-      <button type="button" onClick={changeTheme}>Set theme</button>
-      <h1 className={styles.title}>test</h1>
-    </div>
+  <div className={styles.titleContainer}>
+    <h1>TODO</h1>
+    <button type="button" onClick={changeTheme}>
+      <img alt="change theme icon" src={theme === 'light' ? moonIcon : sunIcon} />
+    </button>
   </div>
 );
 
